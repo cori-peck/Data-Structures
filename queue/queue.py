b95bf686e4b086dcc0bb09d24c8f6e97205f801d
cori-peck <cori.peck@ymail.com>
2019-08-19 18:20:28 -0500

    trying to add todays material into queue, but getting errors

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,49 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value 
+    self.next_node = next_node
+
+  def make_next(self, new_next):
+    self.next_node = new_next
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    
+    def add_to_tail(self, value):
+      new_node = Node(value)
+      
+      if not self.head and not self.tail:
+        self.head = new_node
+        self.tail = new_node
+      else:
+        new_node.prev = self.tail
+        self.tail.make_next(new_node)
+        self.tail = new_node
+
+
+    def remove_from_head(self):
+      value = self.head.value
+      self.delete(self.head)
+      return value
+
+
+
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.size += 1
+    return self.storage.add_to_tail(item)
   
   def dequeue(self):
     pass
 
   def len(self):
-    pass
+    return self.size
