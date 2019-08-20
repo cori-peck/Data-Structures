8ff9fa9d15bbad899d0a71c0cb8a8d39c0bc3908
cori-peck <cori.peck@ymail.com>
2019-08-19 23:17:06 -0500

    I give up... I don't why it's not working.

--- queue/queue.py
+++ queue/queue.py
@@ -1,49 +1,23 @@
 class Node:
   def __init__(self, value, next_node=None):
-    self.value = value 
+    self.value = value
     self.next_node = next_node
-
-  def make_next(self, new_next):
-    self.next_node = new_next
-
-class LinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
-    
-    def add_to_tail(self, value):
-      new_node = Node(value)
-      
-      if not self.head and not self.tail:
-        self.head = new_node
-        self.tail = new_node
-      else:
-        new_node.prev = self.tail
-        self.tail.make_next(new_node)
-        self.tail = new_node
-
-
-    def remove_from_head(self):
-      value = self.head.value
-      self.delete(self.head)
-      return value
-
-
-
 
 class Queue:
   def __init__(self):
     self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = LinkedList()
+    self.storage = []
 
   def enqueue(self, item):
+    self.storage.append(item)
     self.size += 1
-    return self.storage.add_to_tail(item)
-  
+
   def dequeue(self):
-    pass
+    if self.size > 0:
+      self.size -= 1
+      return self.storage.pop(0)
+    else:
+      return None
 
   def len(self):
     return self.size
