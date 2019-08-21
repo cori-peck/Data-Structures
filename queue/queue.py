67eeeddb602be075c37e394d09a46a37d67e9646
cori-peck <cori.peck@ymail.com>
2019-08-20 22:42:36 -0500

    should fix queue but still getting errors?

--- queue/queue.py
+++ queue/queue.py
@@ -1,23 +1,21 @@
-class Node:
-  def __init__(self, value, next_node=None):
-    self.value = value
-    self.next_node = next_node
+from doubly_linked_list import DoublyLinkedList
 
 class Queue:
   def __init__(self):
     self.size = 0
-    self.storage = []
+    self.storage = DoublyLinkedList()
 
   def enqueue(self, item):
-    self.storage.append(item)
+    self.storage.add_to_tail(item)
     self.size += 1
 
   def dequeue(self):
     if self.size > 0:
       self.size -= 1
-      return self.storage.pop(0)
     else:
       return None
+
+    return self.storage.remove_from_head()
 
   def len(self):
     return self.size
