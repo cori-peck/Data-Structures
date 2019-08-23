f1092f95d3bcb9e5acbdd9998863a8f2cb2deaf4
cori-peck <cori.peck@ymail.com>
2019-08-22 19:13:07 -0500

    FIXED! all tests pass, MVP complete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,11 +3,13 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    deleted_item = self.storage[0]
-    del self.storage[0]
+    last_index = self.get_size()-1
+    self.storage[0], self.storage[last_index] = self.storage[last_index], self.storage[0]
+    deleted_item = self.storage.pop()
     self._sift_down(0)
     return deleted_item
 
@@ -18,22 +20,24 @@ class Heap:
     return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    while index > 0:
+      parent = (index - 1) // 2
+      if (self.storage[index] > self.storage[parent] and index > 0):
+        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+        index = parent
+
+      else:
+        break
 
   def _sift_down(self, index):
     left = (2 * index) + 1
     right = (2 * index) + 2
-    max_heap = index
-    if len(self.storage) > left and self.storage[max_heap] < self.storage[left]:
-      max_heap = left
-    if len(self.storage) > right and self.storage[max_heap] < self.storage[right]:
-      max_heap = right
-    if max_heap == index:
-      return
 
-    else:
-      sub = self.storage[index]
-      self.storage[index] = self.storage[max_heap]
-      self.storage[max_heap] = sub
-      self._sift_down(max_heap)
+    if left < self.get_size():
+      if right < self.get_size():
+        if self.storage[left] < self.storage[right]:
+          left = right
+        if self.storage[index] < self.storage[left]:
+          self.storage[index], self.storage[left] = self.storage[left], self.storage[index]
+          self._sift_down(left)
 
